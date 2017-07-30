41df15e37161fa888104434660bdafd6ba07ea42
Morten Rasmussen <morten@tildeslash.dk>
2017-07-30 16:16:01 +0100

    Introduces a TokenValidator interface

--- tokenValidator.go
+++ tokenValidator.go
@@ -7,17 +7,21 @@ import (
 
 // TokenValidator is a struct to hold validators used to validate
 // a JWT against the rules set out by the Firebase project.
-type TokenValidator struct {
+type TokenValidatorImpl struct {
 	projectID          string
 	headerValidator    HeaderValidator
 	claimsValidator    ClaimsValidator
 	signatureValidator SignatureValidator
 }
 
+type TokenValidator interface {
+	Validate(token string) (bool, error)
+}
+
 // NewDefaultTokenValidator is the default token validator that validates using the
 // DefaultHeaderValidator, DefaultClaimsValidator and DefaultSignatureValidator
 // to validate a token against the rules set out by the Firebase projects documentation.
-func NewDefaultTokenValidator(projectID string) *TokenValidator {
+func NewDefaultTokenValidator(projectID string) TokenValidator {
 	return NewTokenValidator(projectID,
 		&DefaultHeaderValidator{},
 		NewDefaultClaimsValidator(),
@@ -27,15 +31,15 @@ func NewDefaultTokenValidator(projectID string) *TokenValidator {
 // NewTokenValidator allows you to customize the TokenValidator by substituting
 // validators for the individual JWT segments. See the validator interfaces
 // for implementation details on the specific validators.
-func NewTokenValidator(projectID string, headerValidator HeaderValidator, claimsValidator ClaimsValidator, signatureValidator SignatureValidator) *TokenValidator {
-	t := &TokenValidator{projectID: projectID, headerValidator: headerValidator, claimsValidator: claimsValidator, signatureValidator: signatureValidator}
+func NewTokenValidator(projectID string, headerValidator HeaderValidator, claimsValidator ClaimsValidator, signatureValidator SignatureValidator) TokenValidator {
+	t := &TokenValidatorImpl{projectID: projectID, headerValidator: headerValidator, claimsValidator: claimsValidator, signatureValidator: signatureValidator}
 	return t
 }
 
 // Validate a jwt token against the rules set out in the TokenValidators three validators.
 // Return result of the validation and an error telling which part of the validation went
 // wrong if the result is false.
-func (tv *TokenValidator) Validate(token string) (bool, error) {
+func (tv *TokenValidatorImpl) Validate(token string) (bool, error) {
 	split := strings.Split(token, ".")
 
 	if len(split) != 3 {
